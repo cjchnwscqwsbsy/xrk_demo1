d41e567da23daabddac6679c973cef7f45a6d3d9
rkxie <rkxie@amarsoft.com>
2019-03-26 14:11:03 +0800

    feat:a mobx test

--- src/container/app.js
+++ src/container/app.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 
+@observer
 export default  class App extends React.Component {
     render() {
         return (
-            <div>测试</div>
+            <button onClick={this.onReset.bind(this)}>
+                Seconds passed: {this.props.appState.timer}
+            </button>
         );
+    }
+
+    onReset() {
+        this.props.appState.resetTimer();
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { observable, action } from 'mobx';
 import App from './container/app';
 
-ReactDom.render(<App />, document.getElementById('app'));
+const appState = observable({
+    timer: 0
+});
+
+appState.resetTimer = action(function reset() {
+    appState.timer = 0;
+});
+
+setInterval(action(function tick() {
+    appState.timer += 1;
+}), 1000);
+
+ReactDom.render(<App appState={appState}/>, document.getElementById('app'));
