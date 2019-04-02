f7678f92defccadb219dd822d86af162a1336300
rkxie <rkxie@amarsoft.com>
2019-04-02 15:05:44 +0800

    feat:a mobx test

--- src/container/app.js
+++ src/container/app.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import './app.less';
 
 @observer
 export default  class App extends React.Component {
     render() {
         return (
-            <button onClick={this.onReset.bind(this)}>
-                Seconds passed: {this.props.appState.timer}
-            </button>
+            <div className={`app-container`}>
+                <button onClick={this.onReset.bind(this)}>
+                    Seconds passed: {this.props.appState.timer}
+                </button>
+                <div className={`app-container-page`}>
+                    page
+                    <div className={`app-container-page-child`}>child page</div>
+                </div>
+            </div>
         );
     }
 
