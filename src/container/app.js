63033dd6653d234df3e31e5713e3965a6ecf7e2b
rkxie <rkxie@amarsoft.com>
2019-04-08 09:02:14 +0800

    remote un-use code

--- src/container/app.js
+++ src/container/app.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import { observer } from 'mobx-react';
-import './app.less';
 
-@observer
 export default  class App extends React.Component {
     render() {
         return (
             <div className={`app-container`}>
-                <button onClick={this.onReset.bind(this)}>
-                    Seconds passed: {this.props.appState.timer}
-                </button>
-                <div className={`app-container-page`}>
-                    page
-                    <div className={`app-container-page-child`}>child page</div>
-                </div>
+                4567890
             </div>
         );
-    }
-
-    onReset() {
-        this.props.appState.resetTimer();
     }
 }
