1efbd3f4a18c9ec604ab526814a4af354c533339
rkxie <rkxie@amarsoft.com>
2019-04-14 18:22:31 +0800

    add split code

--- src/container/app.js
+++ src/container/app.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import ReactRouter from 'react-router';
+import ReactRouterDom from 'react-router-dom';
 
 export default  class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.myRef = React.createRef();
+    }
+    componentDidMount(){
+        console.log('xrk:',this.myRef,this.myRef.current);
+    }
     render() {
         return (
-            <div className={`app-container`}>
+            <div
+                className={`app-container`}
+                ref={this.myRef}
+            >
                 4567890
             </div>
         );
