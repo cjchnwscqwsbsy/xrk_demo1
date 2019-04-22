b9902f6c89166898f9e340d82c8aa7c928529d6a
rkxie <rkxie@amarsoft.com>
2019-04-22 16:16:40 +0800

    mock menu data

--- src/framework/home/index.js
+++ src/framework/home/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './index.less';
+import { menuData } from './mock';
 
 export default class Home extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            dataSource:[]
+        };
+    }
+    componentDidMount(){
+        this.setState({
+            dataSource:menuData
+        });
+    }
     render(){
+        const { dataSource } = this.state;
         return (
             <div className={`framework-home`}>
                 <div className={`framework-home-header`}>
@@ -10,8 +23,13 @@ export default class Home extends React.Component{
                 </div>
                 <div className={`framework-home-content`}>
                     <div className={`framework-home-content-box`}>
-                        <div className={`framework-home-content-box-menu`}>left</div>
-                        <div className={`framework-home-content-box-showroom`}>right</div>
+                        <div className={`framework-home-content-box-menu`}>
+                            {dataSource.map(item => <span key={item.key}>{item.title}</span>)}
+                        </div>
+                        <div className={`framework-home-content-box-showroom`}>
+                            <div className={`framework-home-content-box-showroom-slide`}>轮播图</div>
+                            <div className={`framework-home-content-box-showroom-hatch`}>下栏导航</div>
+                        </div>
                     </div>
                 </div>
             </div>
