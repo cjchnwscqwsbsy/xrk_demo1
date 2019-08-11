9186ddf74e5308bc44131da956aef738d5de777e
rkxie <rkxie@amarsoft.com>
2019-08-11 11:03:23 +0800

    feat:update code

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
@@ -8,6 +10,37 @@ module.exports = {
     entry:{
         app:path.resolve(__dirname, '../src/index.js'),
         vendors:['react', 'react-dom', 'antd', 'react-router-dom']
+    },
+    optimization:{
+        minimizer:[
+            new UglifyJsPlugin({
+                include: /src/,
+                uglifyOptions:{
+                    compress:{
+                        // 在UglifyJs删除没有用到的代码时不输出警告
+                        warnings: false,
+                        // 删除所有的 `console` 语句，可以兼容ie浏览器
+                        drop_console: true,
+                        // 内嵌定义了但是只用到一次的变量
+                        collapse_vars: true,
+                        // 提取出出现多次但是没有定义成变量去引用的静态值
+                        reduce_vars: true,
+                    },
+                    output:{
+                        //最紧凑的输出
+                        beautify:false,
+                        //删除所有注释
+                        comments:false
+                    }
+                }
+            }),
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorOptions:{
+                    safe:true
+                }
+            })
+        ],
+        splitChunks:{chunks:'all'}
     },
     output: {
         filename: '[name]-[hash:8].js',
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,44 +2,11 @@ const path = require('path');
 const merge=require('webpack-merge');
 const autoprefixer=require('autoprefixer');
 const common = require('./webpack.config');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common,{
     mode:'production',
     devtool:'none',
-    optimization:{
-        minimizer:[
-            new UglifyJsPlugin({
-                include: /src/,
-                uglifyOptions:{
-                    compress:{
-                        // 在UglifyJs删除没有用到的代码时不输出警告
-                        warnings: false,
-                        // 删除所有的 `console` 语句，可以兼容ie浏览器
-                        drop_console: true,
-                        // 内嵌定义了但是只用到一次的变量
-                        collapse_vars: true,
-                        // 提取出出现多次但是没有定义成变量去引用的静态值
-                        reduce_vars: true,
-                    },
-                    output:{
-                        //最紧凑的输出
-                        beautify:false,
-                        //删除所有注释
-                        comments:false
-                    }
-                }
-            }),
-            new OptimizeCSSAssetsPlugin({
-                cssProcessorOptions:{
-                    safe:true
-                }
-            })
-        ],
-        splitChunks:{chunks:'all'}
-    },
     plugins:[
         new MiniCssExtractPlugin({
             filename:'[name].[contenthash:8].css',
