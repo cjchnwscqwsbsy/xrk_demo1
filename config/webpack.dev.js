d6a887f88337f4b1a564fe4b241e1d4eb1c3a106
rkxie <rkxie@amarsoft.com>
2019-08-11 11:05:49 +0800

    feat:update config file

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,6 +1,8 @@
 const path=require('path');
 const merge=require('webpack-merge');
 const autoprefixer=require('autoprefixer');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const common = require('./webpack.config');
 const serverConfig = require('../src/framework/profile');
 
@@ -50,6 +52,37 @@ module.exports = merge(common,{
                 }
             }
         }
+    },
+    optimization:{
+        minimizer:[
+            new UglifyJsPlugin({
+                include: /src/,
+                uglifyOptions:{
+                    compress:{
+                        // 在UglifyJs删除没有用到的代码时不输出警告
+                        warnings: false,
+                        // 删除所有的 `console` 语句，可以兼容ie浏览器
+                        drop_console: true,
+                        // 内嵌定义了但是只用到一次的变量
+                        collapse_vars: true,
+                        // 提取出出现多次但是没有定义成变量去引用的静态值
+                        reduce_vars: true,
+                    },
+                    output:{
+                        //最紧凑的输出
+                        beautify:false,
+                        //删除所有注释
+                        comments:false
+                    }
+                }
+            }),
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorOptions:{
+                    safe:true
+                }
+            })
+        ],
+        splitChunks:{chunks:'all'}
     },
     module:{
         rules:[{
