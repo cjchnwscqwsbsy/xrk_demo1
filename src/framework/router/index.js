33e8692ffcc30c126c57d0129737b88b778591a7
cjchnwscqwsbsy <18506166570@163.com>
2020-07-18 22:20:43 +0800

    test data table

--- src/framework/router/index.js
+++ src/framework/router/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../home';
 import PersonaManager from '../personalManager';
-import Login from '../login';
 import { UploadF } from '../../component/index';
 import Main from '../../framework/main';
 import Radar from '../radar';
@@ -14,7 +13,6 @@ const Router = () => {
             <Route path="/Main" exact component={Main} />
             <Route path="/home" component={Home} />
             <Route path="/reset_password" component={PersonaManager} />
-            <Route path="/login" component={Login} />
             <Route path="/upload" component={UploadF} />
         </BrowserRouter>
     );
